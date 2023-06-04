1b9cf75068ced56bd8ab5f2bea62b85dc0bc02f6
DanielaButkovska <daniela.butkovska@student.um.si>
2023-06-04 18:36:28 +0200

    Import Necessary Libraries

--- faceID.py
+++ faceID.py
@@ -1,2 +1,11 @@
+import numpy as np
 import cv2
-import numpy
+import tensorflow as tf
+from joblib import load
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import ZeroPadding2D, Convolution2D, MaxPooling2D, Dropout, Flatten, Activation
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from facenet_pytorch import MTCNN
