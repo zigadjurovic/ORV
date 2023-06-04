9c00390cb67a3d000946192d99a0e4db49db241c
viktordenac <viktor.denac@gmail.com>
2023-06-04 14:49:44 +0200

    Capturing needed photos for reference (model)

---
+++ captureFace.py
@@ -0,0 +1,7 @@
+import cv2
+
+# Initialize the camera
+cap = cv2.VideoCapture(0)  # Use 0 as the argument for the default camera
+
+# Define the output directory to save the captured frames
+output_dir = 'captured_frames/viktor/viktor'
