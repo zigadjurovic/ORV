c4a5bc2af4bc1bff527187e5eb3ef186fb18b847
viktordenac <viktor.denac@gmail.com>
2023-06-04 14:54:28 +0200

    Adding frame count and releasing camera

--- captureFace.py
+++ captureFace.py
@@ -5,3 +5,11 @@ cap = cv2.VideoCapture(0)  # Use 0 as the argument for the default camera
 
 # Define the output directory to save the captured frames
 output_dir = 'captured_frames/viktor/viktor'
+
+# Initialize frame count and capture count
+frame_count = 0
+capture_count = 0
+
+# Release the camera capture object and close any open windows
+cap.release()
+cv2.destroyAllWindows()
